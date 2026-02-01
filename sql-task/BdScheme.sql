6ed4ca5c955d8307cebc95be8f662a5c3ceb6c84
Ballofmoss <ivansvenn@ya.ru>
2026-02-01 16:29:00 +0400

    Interface task

--- sql-task/BdScheme.sql
+++ sql-task/BdScheme.sql
@@ -2,8 +2,8 @@ CREATE TABLE Car (
     car_id INT PRIMARY KEY AUTO_INCREMENT,
     brand VARCHAR(50) NOT NULL,
     model VARCHAR(50) NOT NULL,
-    engine_volume DECIMAL(3,1) NOT NULL, -- например, 2.5
-    drive_type VARCHAR(20) NOT NULL, -- 'полный', 'передний', 'задний'
+    engine_volume DECIMAL(3,1) NOT NULL,
+    drive_type VARCHAR(20) NOT NULL,
     country_of_origin VARCHAR(50) NOT NULL,
     purchase_price DECIMAL(12,2) NOT NULL
 );
@@ -32,12 +32,12 @@ CREATE TABLE Employee (
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
     position VARCHAR(100) NOT NULL,
-    commission_rate DECIMAL(5,2) NOT NULL -- например, 15.00
+    commission_rate DECIMAL(5,2) NOT NULL 
 );
 
 CREATE TABLE Sale (
     sale_id INT PRIMARY KEY AUTO_INCREMENT,
-    instance_id INT UNIQUE NOT NULL, -- Ключевое ограничение для продажи 1 раз
+    instance_id INT UNIQUE NOT NULL, 
     client_id INT NOT NULL,
     employee_id INT NOT NULL,
     sale_date DATE NOT NULL,
